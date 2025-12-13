a2d1bd65d10c86a2ab97e3c66058ec0e63d746f9
Maxim Belyi <btf9458@gmail.com>
2025-12-13 14:06:20 +0500

    Добавлена галерея about

--- about/index.php
+++ about/index.php
@@ -1,14 +1,14 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Ulitka");
-$APPLICATION->SetPageProperty("inner", "О нас");
+$APPLICATION->SetPageProperty("inner", "О компании");
 ?>
 <main>
     <div class="about">
         <div class="wrapper">
             <div class="page-top">
-                <div class="breadcrumbs"><a class="breadcrumbs__item" href="#">Ulitka</a><a class="breadcrumbs__item"
-                        href="#">О компании</a>
+                <div class="breadcrumbs">
+                    <a class="breadcrumbs__item" href="#">Ulitka</a><a class="breadcrumbs__item" href="#">О компании</a>
                 </div>
                 <h1 class="page-top__title">
                     <? $APPLICATION->IncludeComponent(
@@ -16,9 +16,8 @@ $APPLICATION->SetPageProperty("inner", "О нас");
                         "template1",
                         array(
                             "AREA_FILE_SHOW" => "file",
-                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/section_title.php",
-                        ),
-                        false
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/section_title.php"
+                        )
                     ); ?>
                 </h1>
                 <div class="page-top__author">
@@ -27,23 +26,21 @@ $APPLICATION->SetPageProperty("inner", "О нас");
                         "template1",
                         array(
                             "AREA_FILE_SHOW" => "file",
-                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/author.php",
-                        ),
-                        false
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/author.php"
+                        )
                     ); ?>
                 </div>
             </div>
             <div class="about__top">
                 <div class="about__snail">
                     <? $APPLICATION->IncludeComponent(
-                            "bitrix:main.include",
-                            "template1",
-                            array(
-                                "AREA_FILE_SHOW" => "file",
-                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_img.php",
-                            ),
-                            false
-                        ); ?>
+                        "bitrix:main.include",
+                        "template1",
+                        array(
+                            "AREA_FILE_SHOW" => "file",
+                            "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_img.php"
+                        )
+                    ); ?>
                 </div>
                 <div class="about__text">
                     <div class="about__text-author">
@@ -52,9 +49,8 @@ $APPLICATION->SetPageProperty("inner", "О нас");
                             "template1",
                             array(
                                 "AREA_FILE_SHOW" => "file",
-                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/author.php",
-                            ),
-                            false
+                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/author.php"
+                            )
                         ); ?>
                     </div>
                     <div class="about__text-text">
@@ -63,71 +59,130 @@ $APPLICATION->SetPageProperty("inner", "О нас");
                             "template1",
                             array(
                                 "AREA_FILE_SHOW" => "file",
-                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_text.php",
-                            ),
-                            false
+                                "PATH" => SITE_TEMPLATE_PATH . "/includes/about/about_text.php"
+                            )
                         ); ?>
                     </div>
                 </div>
             </div>
             <div class="about__gallery">
-                <div class="about__gallery-item"><img src="./img/about/gallery-1.jpg" alt="" loading="lazy"></div>
-                <div class="about__gallery-item"><img src="./img/about/gallery-2.jpg" alt="" loading="lazy"></div>
-                <div class="about__gallery-item"><img src="./img/about/gallery-3.jpg" alt="" loading="lazy"></div>
-                <div class="about__gallery-item"><img src="./img/about/gallery-4.jpg" alt="" loading="lazy"></div>
+         
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        "about_gallery",
+                        array(
+                            "IBLOCK_ID" => "7",           
+                            "NEWS_COUNT" => "4",          
+                            "SORT_BY1" => "SORT",        
+                            "SORT_ORDER1" => "DESC",       
+                            "SORT_BY2" => "ID",
+                            "SORT_ORDER2" => "ASC",
+                            "CACHE_TYPE" => "A",        
+                            "CACHE_TIME" => "36000000",   
+                            "CACHE_GROUPS" => "Y",
+                            "SET_TITLE" => "N",           
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N", 
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N"
+                        )
+                    ); ?>
             </div>
+            
             <div class="about__stats">
                 <div class="a-stats">
                     <div class="a-stats__item">
-                        <div class="a-stats__item-title">2005</div>
-                        <div class="a-stats__item-value">год основания компании</div>
+                        <div class="a-stats__item-title">
+                            2005
+                        </div>
+                        <div class="a-stats__item-value">
+                            год основания компании
+                        </div>
                     </div>
                     <div class="a-stats__item">
-                        <div class="a-stats__item-title">&gt;1000</div>
-                        <div class="a-stats__item-value">клиентов по всей России</div>
+                        <div class="a-stats__item-title">
+                            &gt;1000
+                        </div>
+                        <div class="a-stats__item-value">
+                            клиентов по всей России
+                        </div>
                     </div>
                     <div class="a-stats__item">
-                        <div class="a-stats__item-title">2593м2</div>
-                        <div class="a-stats__item-value">площадь производства</div>
+                        <div class="a-stats__item-title">
+                            2593м2
+                        </div>
+                        <div class="a-stats__item-value">
+                            площадь производства
+                        </div>
                     </div>
                     <div class="a-stats__item">
-                        <div class="a-stats__item-title">100ед/день</div>
-                        <div class="a-stats__item-value">производственная мощность</div>
+                        <div class="a-stats__item-title">
+                            100ед/день
+                        </div>
+                        <div class="a-stats__item-value">
+                            производственная мощность
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="about__video" data-video>
-                <div class="about__video-img"><img src="./img/about/video.jpg" alt="" loading="lazy"></div>
-                <div class="about__video-item"><iframe src="https://www.youtube.com/embed/KPXmZ6bLo5w"
+            <div class="about__video" data-video="">
+                <div class="about__video-img">
+                    <img src="./img/about/video.jpg" alt="" loading="lazy">
+                </div>
+                <div class="about__video-item">
+                    <iframe src="https://www.youtube.com/embed/KPXmZ6bLo5w"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe></div>
+                        allowfullscreen></iframe>
+                </div>
             </div>
             <div class="about__direction">
-                <div class="about__direction-title">4 направления нашей работы</div>
-                <div class="a-direction"> <a class="a-direction__item" href="#">
-                        <div class="a-direction__item-img"><img src="./img/about/direction-1.jpg" alt="" loading="lazy">
+                <div class="about__direction-title">
+                    4 направления нашей работы
+                </div>
+                <div class="a-direction">
+                    <a class="a-direction__item" href="#">
+                        <div class="a-direction__item-img">
+                            <img src="./img/about/direction-1.jpg" alt="" loading="lazy">
                         </div>
-                        <div class="a-direction__item-title">Душевые ограждения </div>
-                        <div class="a-direction__item-description">Наша компания уже более 20 лет присутствует на
-                            российском рынке как крупный производитель мебели из металла, стекла и дерева.</div>
+                        <div class="a-direction__item-title">
+                            Душевые ограждения
+                        </div>
+                        <div class="a-direction__item-description">
+                            Наша компания уже более 20 лет присутствует на российском рынке как крупный производитель
+                            мебели из металла, стекла и дерева.
+                        </div>
                     </a><a class="a-direction__item" href="#">
-                        <div class="a-direction__item-img"><img src="./img/about/direction-2.jpg" alt="" loading="lazy">
+                        <div class="a-direction__item-img">
+                            <img src="./img/about/direction-2.jpg" alt="" loading="lazy">
                         </div>
-                        <div class="a-direction__item-title">Поддоны </div>
-                        <div class="a-direction__item-description">Наша компания уже более 20 лет присутствует на
-                            российском рынке как крупный производитель мебели из металла, стекла и дерева.</div>
+                        <div class="a-direction__item-title">
+                            Поддоны
+                        </div>
+                        <div class="a-direction__item-description">
+                            Наша компания уже более 20 лет присутствует на российском рынке как крупный производитель
+                            мебели из металла, стекла и дерева.
+                        </div>
                     </a><a class="a-direction__item" href="#">
-                        <div class="a-direction__item-img"><img src="./img/about/direction-3.jpg" alt="" loading="lazy">
+                        <div class="a-direction__item-img">
+                            <img src="./img/about/direction-3.jpg" alt="" loading="lazy">
                         </div>
-                        <div class="a-direction__item-title">Мебель</div>
-                        <div class="a-direction__item-description">Наша компания уже более 20 лет присутствует на
-                            российском рынке как крупный производитель мебели из металла, стекла и дерева.</div>
+                        <div class="a-direction__item-title">
+                            Мебель
+                        </div>
+                        <div class="a-direction__item-description">
+                            Наша компания уже более 20 лет присутствует на российском рынке как крупный производитель
+                            мебели из металла, стекла и дерева.
+                        </div>
                     </a><a class="a-direction__item" href="#">
-                        <div class="a-direction__item-img"><img src="./img/about/direction-4.jpg" alt="" loading="lazy">
+                        <div class="a-direction__item-img">
+                            <img src="./img/about/direction-4.jpg" alt="" loading="lazy">
                         </div>
-                        <div class="a-direction__item-title">Аксессуары</div>
-                        <div class="a-direction__item-description">Наша компания уже более 20 лет присутствует на
-                            российском рынке как крупный производитель мебели из металла, стекла и дерева.</div>
+                        <div class="a-direction__item-title">
+                            Аксессуары
+                        </div>
+                        <div class="a-direction__item-description">
+                            Наша компания уже более 20 лет присутствует на российском рынке как крупный производитель
+                            мебели из металла, стекла и дерева.
+                        </div>
                     </a>
                 </div>
             </div>
@@ -135,27 +190,50 @@ $APPLICATION->SetPageProperty("inner", "О нас");
                 <div class="typical__slider">
                     <div class="swiper">
                         <div class="swiper-wrapper">
-                            <div class="swiper-slide"><img src="./img/typical/banner.jpg" alt="" loading="lazy"></div>
-                            <div class="swiper-slide"><img src="./img/typical/banner.jpg" alt="" loading="lazy"></div>
-                            <div class="swiper-slide"><img src="./img/typical/banner.jpg" alt="" loading="lazy"></div>
-                            <div class="swiper-slide"><img src="./img/typical/banner.jpg" alt="" loading="lazy"></div>
-                            <div class="swiper-slide"><img src="./img/typical/banner.jpg" alt="" loading="lazy"></div>
+                            <div class="swiper-slide">
+                                <img src="./img/typical/banner.jpg" alt="" loading="lazy">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="./img/typical/banner.jpg" alt="" loading="lazy">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="./img/typical/banner.jpg" alt="" loading="lazy">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="./img/typical/banner.jpg" alt="" loading="lazy">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="./img/typical/banner.jpg" alt="" loading="lazy">
+                            </div>
                         </div>
-                        <div class="swiper-pagination"> </div>
+                        <div class="swiper-pagination">
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="about__help">
                 <div class="a-help">
-                    <div class="a-help__title">Помогаем <br> в нестандартных <br> изделиях</div>
-                    <div class="a-help__img"><img src="./img/about/help.jpg" alt="" loading="lazy"></div>
+                    <div class="a-help__title">
+                        Помогаем <br>
+                        в нестандартных <br>
+                        изделиях
+                    </div>
+                    <div class="a-help__img">
+                        <img src="./img/about/help.jpg" alt="" loading="lazy">
+                    </div>
                     <div class="a-help__text">
-                        <div class="a-help__text-title">Помогаем <br> в нестандартных <br> изделиях</div>
+                        <div class="a-help__text-title">
+                            Помогаем <br>
+                            в нестандартных <br>
+                            изделиях
+                        </div>
                         <div class="a-help__text-text">
-                            <p>Наша компания уже более 20 лет присутствует на российском рынке как крупный производитель
-                                мебели из металла, стекла и дерева. В рамках освоения новых горизонтов в 200_ году было
-                                запущено производство душевых ограждений, мебели и аксессуаров для ванной комнаты,
-                                которое преобразовалось в молодой и перспективный бренд Ulitka. </p>
+                            <p>
+                                Наша компания уже более 20 лет присутствует на российском рынке как крупный
+                                производитель мебели из металла, стекла и дерева. В рамках освоения новых горизонтов в
+                                200_ году было запущено производство душевых ограждений, мебели и аксессуаров для ванной
+                                комнаты, которое преобразовалось в молодой и перспективный бренд Ulitka.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -163,5 +241,3 @@ $APPLICATION->SetPageProperty("inner", "О нас");
         </div>
     </div>
 </main>
-<?php
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
---
+++ local/templates/giga/components/bitrix/news.list/about_gallery/template.php
@@ -0,0 +1,39 @@
+<!-- <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die(); ?>
+
+<?php if (!empty($arResult["ITEMS"])): ?>
+
+    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
+        <?php
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+
+        <div class="about__gallery-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+            <?php $arItem["PREVIEW_PICTURE"] ?>
+        </div>
+    <?php endforeach; ?>
+    </div>
+<?php endif; ?> -->
+
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<?php if (!empty($arResult["ITEMS"])): ?>
+    
+    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
+        <?php
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+
+        <div class="about__gallery-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+            <?php if (!empty($arItem["PREVIEW_PICTURE"]["SRC"])): ?>
+                <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" 
+                     alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"] ?: $arItem["NAME"]?>" 
+                     loading="lazy">
+            <?php endif; ?>
+        </div>
+
+    <?php endforeach; ?>
+
+<?php endif; ?>
