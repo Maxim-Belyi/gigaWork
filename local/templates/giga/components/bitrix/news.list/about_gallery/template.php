46b2d0ef73e59e4f834671fa1a0f09145ef864b2
Maxim Belyi <btf9458@gmail.com>
2025-12-13 14:06:36 +0500

    добавлена галерея about

--- local/templates/giga/components/bitrix/news.list/about_gallery/template.php
+++ local/templates/giga/components/bitrix/news.list/about_gallery/template.php
@@ -1,21 +1,3 @@
-<!-- <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
-    die(); ?>
-
-<?php if (!empty($arResult["ITEMS"])): ?>
-
-    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-        <?php
-        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-        ?>
-
-        <div class="about__gallery-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-            <?php $arItem["PREVIEW_PICTURE"] ?>
-        </div>
-    <?php endforeach; ?>
-    </div>
-<?php endif; ?> -->
-
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <?php if (!empty($arResult["ITEMS"])): ?>
