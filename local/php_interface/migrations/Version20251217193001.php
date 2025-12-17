98d36f5cbed815219c96273c9d608798830d0f82
Maxim Belyi <btf9458@gmail.com>
2025-12-17 21:37:55 +0500

    изменения в файлах миграций

---
+++ local/php_interface/migrations/Version20251217193001.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20251217193001 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "";
+
+    protected $moduleVersion = "5.4.1";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->saveElementByXmlId(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setLimit(10)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                     ->Iblock()
+                     ->deleteElementByXmlId(
+                         $item['iblock_id'],
+                         $item['fields']['XML_ID']
+                     );
+             });
+    }
+}
